c6cac04f3ecb87f75582f4d6074b56a0ac408f23
Olivier Cervello <olivier.cervello@gmail.com>
2023-02-27 08:45:04 +0000

    move dalfox up
    Former-commit-id: caa7bc597e4345c5c23dc7a7ce67e359627eaf00

--- secsy/tools/vuln.py
+++ secsy/tools/vuln.py
@@ -70,6 +70,45 @@ class VulnCommand(CommandRunner):
 		return item
 
 
+class dalfox(VulnCommand):
+	"""DalFox is a powerful open source XSS scanning tool."""
+	cmd = 'dalfox'
+	input_type = URL
+	input_flag = 'url'
+	file_flag = 'file'
+	json_flag = '--format json'
+	opt_prefix = '--'
+	opt_key_map = {
+		HEADER: 'header',
+		DELAY: 'delay',
+		FOLLOW_REDIRECT: 'follow-redirects',
+		METHOD: 'method',
+		PROXY: 'proxy',
+		RATE_LIMIT: OPT_NOT_SUPPORTED,
+		THREADS: OPT_NOT_SUPPORTED,
+		TIMEOUT: 'timeout',
+		USER_AGENT: 'user-agent'
+	}
+	output_map = {
+		VULN_ID: 'XSS Injection',
+		VULN_NAME: 'XSS Injection',
+		VULN_PROVIDER: 'dalfox',
+		VULN_TAGS: lambda x: [x['cwe']],
+		VULN_CONFIDENCE: 'high',
+		VULN_MATCHED_AT: lambda x: urlparse(x['data'])._replace(query='').geturl(),
+		VULN_EXTRACTED_RESULTS: lambda x: {
+			k: v for k, v in x.items()
+			if k not in ['type', 'severity', 'cwe']
+		},
+		VULN_SEVERITY: lambda x: x['severity'].lower()
+	}
+
+	@staticmethod
+	def on_line(self, line):
+		line = line.rstrip(',')
+		return line
+
+
 class nmap(VulnCommand):
 	"""Network Mapper is a free and open source utility for network discovery
 	and security auditing."""
@@ -517,45 +556,6 @@ class nuclei(VulnCommand):
 		if len(cve_ids) > 0:
 			return cve_ids[0]
 		return None
-
-
-class dalfox(VulnCommand):
-	"""DalFox is a powerful open source XSS scanning tool."""
-	cmd = 'dalfox'
-	input_type = URL
-	input_flag = 'url'
-	file_flag = 'file'
-	json_flag = '--format json'
-	opt_prefix = '--'
-	opt_key_map = {
-		HEADER: 'header',
-		DELAY: 'delay',
-		FOLLOW_REDIRECT: 'follow-redirects',
-		METHOD: 'method',
-		PROXY: 'proxy',
-		RATE_LIMIT: OPT_NOT_SUPPORTED,
-		THREADS: OPT_NOT_SUPPORTED,
-		TIMEOUT: 'timeout',
-		USER_AGENT: 'user-agent'
-	}
-	output_map = {
-		VULN_ID: 'XSS Injection',
-		VULN_NAME: 'XSS Injection',
-		VULN_PROVIDER: 'dalfox',
-		VULN_TAGS: lambda x: [x['cwe']],
-		VULN_CONFIDENCE: 'high',
-		VULN_MATCHED_AT: lambda x: urlparse(x['data'])._replace(query='').geturl(),
-		VULN_EXTRACTED_RESULTS: lambda x: {
-			k: v for k, v in x.items()
-			if k not in ['type', 'severity', 'cwe']
-		},
-		VULN_SEVERITY: lambda x: x['severity'].lower()
-	}
-
-	@staticmethod
-	def on_line(self, line):
-		line = line.rstrip(',')
-		return line
 
 
 def sort_vulns_by_severity(vulns):
