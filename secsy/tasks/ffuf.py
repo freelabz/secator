623d1392f47eb31c241a365d65c0c249cd0896db
Olivier Cervello <olivier.cervello@gmail.com>
2023-03-13 17:26:05 +0000

    update ffuf args

--- secsy/tasks/ffuf.py
+++ secsy/tasks/ffuf.py
@@ -14,11 +14,13 @@ class ffuf(HTTPCommand):
 	opts = {
 		AUTO_CALIBRATION: {'is_flag': True, 'short': 'ac', 'help': 'Filter out HTTP responses based on status codes, content length, etc.'},
 		WORDLIST: {'type': str, 'short': 'w', 'default': FFUF_DEFAULT_WORDLIST, 'help': 'Wordlist to fuzz from.'},
-		'mw': {'type': str, 'help': 'Match responses by word count'},
-		'mr': {'type': str, 'help': 'Match regex in URL or response body'},
+		'mw': {'type': str, 'help': 'Match responses with word count'},
+		'mr': {'type': str, 'help': 'Match responses with regular expression'},
+		'ms': {'type': str, 'help': 'Match respones with size'},
 		'fc': {'type': str, 'help': 'Filter out responses with HTTP codes'},
 		'fw': {'type': str, 'help': 'Filter out responses with word count'},
-		'fr': {'type': str, 'help': 'Filter out responses matching regular expression'},
+		'fr': {'type': str, 'help': 'Filter out responses with regular expression'},
+		'fs': {'type': str, 'help': 'Filter out responses with size'}
 	}
 	opt_key_map = {
 		HEADER: 'H',
