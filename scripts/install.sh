466e1a65fddc938ae0ddda20e2257c5edeb9488b
Olivier Cervello <olivier.cervello@gmail.com>
2023-03-16 11:27:48 +0100

    Update install.sh
    Former-commit-id: 6b6ec7ad41a701b0b9eb75642dd9b60d418aae8b

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 git clone https://github.com/freelabz/secsy-cli
 cd secsy-cli
 ./scripts/install_go.sh
 ./scripts/install_commands.sh
+pip3 install virtualenv
+virtualenv ~/.secsy/venv
+source ~/.secsy/venv/bin/activate
+pip3 install -r requirements.txt
 pip3 install -e .
