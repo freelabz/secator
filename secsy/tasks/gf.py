db307bb0adaf756e12bebd09706608d70b545b30
Olivier Cervello <olivier.cervello@gmail.com>
2023-04-04 12:25:17 +0000

    fix gf install
    Former-commit-id: c73a3fab472aa4eb310f20d5a967e144e4f043f5

--- secsy/tasks/gf.py
+++ secsy/tasks/gf.py
@@ -1,7 +1,10 @@
+import os
+
 from secsy.definitions import OPT_PIPE_INPUT, URL
 from secsy.output_types import Tag
 from secsy.runners import Command
 
+HOME_DIR = os.path.expanduser('~')
 
 class gf(Command):
 	"""Wrapper around grep, to help you grep for things."""
@@ -16,8 +19,8 @@ class gf(Command):
 	}
 	input_type = URL
 	install_cmd = (
-		'go install -v github.com/tomnomnom/gf@latest && git clone https://github.com/1ndianl33t/Gf-Patterns ~/.gf || '
-		'true'
+		'go install -v github.com/tomnomnom/gf@latest && '
+		f'git clone https://github.com/1ndianl33t/Gf-Patterns {HOME_DIR}/.gf || true'
 	)
 	output_types = [Tag]
 	item_loader = lambda self, line: {'match': line, 'name': self.get_opt_value('pattern')}  # noqa: E731
