1ee3f0e0d14be85c357698c3d5d90d6c924bcad1
Olivier Cervello <olivier.cervello@gmail.com>
2023-04-04 12:57:08 +0000

    update gf

--- secsy/tasks/gf.py
+++ secsy/tasks/gf.py
@@ -4,8 +4,6 @@ from secsy.definitions import OPT_PIPE_INPUT, URL
 from secsy.output_types import Tag
 from secsy.runners import Command
 
-HOME_DIR = os.path.expanduser('~')
-
 
 class gf(Command):
 	"""Wrapper around grep, to help you grep for things."""
@@ -21,7 +19,7 @@ class gf(Command):
 	input_type = URL
 	install_cmd = (
 		'go install -v github.com/tomnomnom/gf@latest && '
-		f'git clone https://github.com/1ndianl33t/Gf-Patterns {HOME_DIR}/.gf || true'
+		f'git clone https://github.com/1ndianl33t/Gf-Patterns $HOME/.gf || true'
 	)
 	output_types = [Tag]
 	item_loader = lambda self, line: {'match': line, 'name': self.get_opt_value('pattern')}  # noqa: E731
