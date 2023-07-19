8040eacceeb21fcf9458ab472d04b77845141133
Olivier Cervello <olivier.cervello@gmail.com>
2023-07-19 13:30:08 +0000

    fix tests
    Former-commit-id: 2d51e79f2f3288ac5fdee2f1f7f126a20fc81964

--- secator/utils_test.py
+++ secator/utils_test.py
@@ -123,6 +123,7 @@ def mock_command(cls, targets=[], opts={}, fixture=None, method=''):
 	else:
 		mocks.append(fixture)
 
+
 	with mock_subprocess_popen(mocks):
 		command = cls(targets, **opts)
 		if method == 'run':
