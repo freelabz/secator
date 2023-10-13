87a46bab39b6b0e7f8162a76610bace430a60e2a
Olivier Cervello <olivier.cervello@gmail.com>
2023-10-13 12:49:24 +0000

    add port state to integration tests

--- tests/integration/outputs.py
+++ tests/integration/outputs.py
@@ -192,8 +192,8 @@ OUTPUTS_TASKS = {
     ],
     'msfconsole': [],
     'naabu': [
-        Port(port=3000, host='localhost', ip='127.0.0.1', _source='naabu'),
-        Port(port=8080, host='localhost', ip='127.0.0.1', _source='naabu'),
+        Port(port=3000, host='localhost', ip='127.0.0.1', state='open', _source='naabu'),
+        Port(port=8080, host='localhost', ip='127.0.0.1', state='open', _source='naabu'),
     ],
     'nmap': [
         Port(port=3000, host='localhost', ip='127.0.0.1', state='open', service_name='ppp', _source='nmap'),
