5f5306a205cc38082fad6b4e6a0273c4de94a48d
Olivier Cervello <ocervello@freelabz.com>
2024-03-06 09:49:13 -0500

    revert to previous go version

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -1,7 +1,7 @@
 sudo rm -rf /usr/local/go
-wget https://golang.org/dl/go1.22.0.linux-amd64.tar.gz
-tar -xvf go1.22.0.linux-amd64.tar.gz
-rm go1.22.0.linux-amd64.tar.gz || true
+wget https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
+tar -xvf go1.20.2.linux-amd64.tar.gz
+rm go1.20.2.linux-amd64.tar.gz || true
 sudo mv go /usr/local
 sudo rm /usr/bin/go
 sudo ln -s /usr/local/go/bin/go /usr/bin/go
