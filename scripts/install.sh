1d303dbec8913d02f2435da4930be798908f0880
Olivier Cervello <ocervello@freelabz.com>
2024-04-05 10:41:39 -0400

    modify install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,19 +3,26 @@ YELLOW='\033[0;93m'
 GREEN='\033[0;92m'
 NC='\033[0m' # No Color
 
-echo -e "🗄 ${YELLOW}Installing pipx ...${NC}"
-sudo apt install pipx
+echo -e "🗄 ${YELLOW}Installing Python and pipx ...${NC}"
+sudo apt install -y python3-pip pipx
 echo -e "🗄 ${GREEN}pipx installed successfully !${NC}\n"
 
-echo -e "🗄 ${YELLOW}Installing secator ...${NC}"
-pipx install secator
-echo -e "🗄 ${GREEN}secator installed successfully !${NC}\n"
+echo -e "🗄 ${YELLOW}Setting \$PATH ...${NC}"
+export PATH=$PATH:~/.local/bin:~/go/bin
+echo -e "🗄 ${GREEN}\$PATH modified successfully !${NC}\n"
 
+echo -e "🗄 ${YELLOW}Installing secator and dependencies ...${NC}"
+pipx install secator
 secator install langs go
 secator install langs ruby
 secator install tools
+secator install addons redis
+secator install addons worker
+secator install addons google
+secator install addons mongodb
+echo -e "🗄 ${GREEN}secator installed successfully !${NC}\n"
 
-echo -e "🗄 ${YELLOW}Adding ~/go/bin and ~/.local/bin to .bashrc ...${NC}"
+echo -e "🗄 ${YELLOW}Adding ~/go/bin and ~/.local/bin to \$PATH in .bashrc ...${NC}"
 echo "export PATH=$PATH:~/go/bin:~/.local/bin" >> ~/.bashrc
 source ~/.bashrc
-echo -e "🗄 ${GREEN}PATH modified successfully !${NC}\n"
+echo -e "🗄 ${GREEN}\$PATH modified successfully !${NC}\n"
