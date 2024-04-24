41d8bb3292997c3e3e110c99636a1c852fdfed11
Olivier Cervello <ocervello@freelabz.com>
2024-04-24 19:27:46 -0400

    update

--- tests/integration/test_worker.py
+++ tests/integration/test_worker.py
@@ -19,25 +19,25 @@ class TestWorker(unittest.TestCase):
 		cls.cmd.process.kill()
 		cls.thread.join()
 
-	def test_httpx(self):
-		from secator.output_types import Url
-		cmd = Command.execute(
-			'secator x httpx testphp.vulnweb.com -json',
-			no_process=False,
-			cls_attributes={'output_types': [Url]}
-		)
-		# self.assertEqual(cmd.return_code, 0)  # TODO: figure out why return code is -9 when running from unittest
-		self.assertEqual(len(cmd.results), 1)
-		url = Url(
-			'http://testphp.vulnweb.com',
-			status_code=200,
-			title='Home of Acunetix Art',
-			webserver='nginx',
-			tech=['DreamWeaver', 'Nginx:1.19.0', 'PHP:5.6.40', 'Ubuntu'],
-			content_type='text/html',
-			content_length=4958
-		)
-		self.assertEqual(cmd.results[0], url)
+	# def test_httpx(self):
+	# 	from secator.output_types import Url
+	# 	cmd = Command.execute(
+	# 		'secator x httpx testphp.vulnweb.com -json',
+	# 		no_process=False,
+	# 		cls_attributes={'output_types': [Url]}
+	# 	)
+	# 	# self.assertEqual(cmd.return_code, 0)  # TODO: figure out why return code is -9 when running from unittest
+	# 	self.assertEqual(len(cmd.results), 1)
+	# 	url = Url(
+	# 		'http://testphp.vulnweb.com',
+	# 		status_code=200,
+	# 		title='Home of Acunetix Art',
+	# 		webserver='nginx',
+	# 		tech=['DreamWeaver', 'Nginx:1.19.0', 'PHP:5.6.40', 'Ubuntu'],
+	# 		content_type='text/html',
+	# 		content_length=4958
+	# 	)
+	# 	self.assertEqual(cmd.results[0], url)
 
 	def test_host_recon(self):
 		from secator.output_types import Url, Port, Vulnerability
@@ -62,7 +62,8 @@ class TestWorker(unittest.TestCase):
 			webserver='nginx/1.19.0',
 			tech=['Nginx:1.19.0'],
 			content_type='text/html',
-			content_length=4018
+			content_length=4018,
+			_source='httpx'
 		)
 		vuln = Vulnerability(
 			name='nginx-version',
@@ -70,9 +71,11 @@ class TestWorker(unittest.TestCase):
 			id='',
 			matched_at='http://vulnweb.com',
 			confidence='high',
+			confidence_nb=4,
+			severity_nb=4,
 			severity='info',
-			cvss_score=0,
-			tags=['tech', 'nginx']
+			tags=['tech', 'nginx'],
+			_source='nuclei'
 		)
 		self.assertIn(port, cmd.results)
 		self.assertIn(url, cmd.results)
