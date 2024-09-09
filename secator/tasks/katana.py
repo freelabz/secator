947e343801f9cb28fb419e48cb609cce3ffd3ce7
Olivier Cervello <olivier.cervello@gmail.com>
2024-09-09 23:01:15 +0200

    Update secator/tasks/katana.py

--- secator/tasks/katana.py
+++ secator/tasks/katana.py
@@ -30,7 +30,7 @@ class katana(HttpCrawler):
 		'headless': {'is_flag': True, 'short': 'hl', 'help': 'Headless mode'},
 		'system_chrome': {'is_flag': True, 'short': 'sc', 'help': 'Use local installed chrome browser'},
 		'form_extraction': {'is_flag': True, 'short': 'fx', 'help': 'Detect forms'},
-		'store_responses': {'is_flag': True, 'short': 'sr', 'default': CONFIG.http.store_responses, 'help': 'Store responses'},
+		'store_responses': {'is_flag': True, 'short': 'sr', 'default': CONFIG.http.store_responses, 'help': 'Store responses'},  # noqa: E501
 		'form_fill': {'type': bool, 'short': 'ff', 'help': 'Enable form filling'}
 	}
 	opt_key_map = {
