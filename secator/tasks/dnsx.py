40f844847ed214285b0f56deb148dc43b9b8597d
Olivier Cervello <ocervello@freelabz.com>
2024-09-13 08:19:22 -0400

    fix: dnsx parsing output loading error

--- secator/tasks/dnsx.py
+++ secator/tasks/dnsx.py
@@ -23,35 +23,33 @@ class dnsx(ReconDns):
 		'resolver': {'type': str, 'short': 'r', 'help': 'List of resolvers to use (file or comma separated)'},
 		'wildcard_domain': {'type': str, 'short': 'wd', 'help': 'Domain name for wildcard filtering'},
 	}
-
+	item_loaders = []
 	install_cmd = 'go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest'
 	install_github_handle = 'projectdiscovery/dnsx'
 	profile = 'io'
 
 	@staticmethod
 	def item_loader(self, line):
-		items = []
 		try:
 			item = json.loads(line)
-			if self.orig:  # original dnsx output
-				return item
-			host = item['host']
-			record_types = ['a', 'aaaa', 'cname', 'mx', 'ns', 'txt', 'srv', 'ptr', 'soa', 'axfr', 'caa']
-			for _type in record_types:
-				values = item.get(_type, [])
-				for value in values:
-					name = value
-					extra_data = {}
-					if isinstance(value, dict):
-						name = value['name']
-						extra_data = {k: v for k, v in value.items() if k != 'name'}
-					items.append({
-						'host': host,
-						'name': name,
-						'type': _type.upper(),
-						'extra_data': extra_data
-					})
-		except json.decoder.JSONDecodeError:
-			pass
-
-		return items
+		except json.JSONEncoder:
+			return
+		if self.orig:  # original dnsx JSON output
+			yield item
+			return
+		host = item['host']
+		record_types = ['a', 'aaaa', 'cname', 'mx', 'ns', 'txt', 'srv', 'ptr', 'soa', 'axfr', 'caa']
+		for _type in record_types:
+			values = item.get(_type, [])
+			for value in values:
+				name = value
+				extra_data = {}
+				if isinstance(value, dict):
+					name = value['name']
+					extra_data = {k: v for k, v in value.items() if k != 'name'}
+				yield {
+					'host': host,
+					'name': name,
+					'type': _type.upper(),
+					'extra_data': extra_data
+				}
