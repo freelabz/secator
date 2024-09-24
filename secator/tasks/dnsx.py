ccd1edfbb92d95ffa1d0c1b27ab90c5f4a99db55
Olivier Cervello <ocervello@freelabz.com>
2024-09-24 09:51:09 +0200

    Update dnsx.py

--- secator/tasks/dnsx.py
+++ secator/tasks/dnsx.py
@@ -9,7 +9,7 @@ import json
 @task()
 class dnsx(ReconDns):
 	"""dnsx is a fast and multi-purpose DNS toolkit designed for running various retryabledns library."""
-  cmd = 'dnsx -resp -a -aaaa -cname -mx -ns -txt -srv -ptr -soa -axfr -caa'
+  	cmd = 'dnsx -resp -a -aaaa -cname -mx -ns -txt -srv -ptr -soa -axfr -caa'
 	json_flag = '-json'
 	input_flag = OPT_PIPE_INPUT
 	file_flag = OPT_PIPE_INPUT
@@ -18,13 +18,11 @@ class dnsx(ReconDns):
 		RATE_LIMIT: 'rate-limit',
 		RETRIES: 'retry',
 		THREADS: 'threads',
-		"reverse": ''
 	}
 	opts = {
 		'trace': {'is_flag': True, 'default': False, 'help': 'Perform dns tracing'},
 		'resolver': {'type': str, 'short': 'r', 'help': 'List of resolvers to use (file or comma separated)'},
 		'wildcard_domain': {'type': str, 'short': 'wd', 'help': 'Domain name for wildcard filtering'},
-		'reverse': {'is_flag': True, 'default': False, 'help': 'Perform reverse dns lookup'},
 	}
 	install_cmd = 'go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest'
 	install_github_handle = 'projectdiscovery/dnsx'
@@ -44,19 +42,18 @@ class dnsx(ReconDns):
 					name = value['name']
 					extra_data = {k: v for k, v in value.items() if k != 'name'}
 				if _type == 'a':
-					yield {
-						'host': host,
-						'ip': name
-					}
-				if _type == 'ptr':
-					yield {
-						'host': name,
-						'domain': extract_root_domain_from_domain(name),
-						'_source': 'dnsx'
-					}
-				yield {
-					'host': host,
-					'name': name,
-					'type': _type.upper(),
-					'extra_data': extra_data
-				}
+					yield Ip(
+						host=host,
+						ip=name
+					)
+				elif _type == 'ptr':
+					yield Subdomain(
+						host=name,
+						domain=extract_root_domain_from_domain(name)
+					)
+				yield Record(
+					host=host,
+					name=name,
+					type=_type.upper(),
+					extra_data=extra_data
+				)
