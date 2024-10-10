be62a46dcc1fc10db9fdd348108574dbba0f14dc
Olivier Cervello <ocervello@freelabz.com>
2024-10-10 13:10:08 -0400

    fix searchsploit

--- secator/tasks/searchsploit.py
+++ secator/tasks/searchsploit.py
@@ -58,14 +58,14 @@ class searchsploit(Command):
 
 	@staticmethod
 	def before_init(self):
-		_in = self.input
+		_in = self.input[0]
 		self.matched_at = None
 		if '~' in _in:
 			split = _in.split('~')
 			self.matched_at = split[0]
-			self.input = split[1]
-		if isinstance(self.input, str):
-			self.input = self.input.replace('httpd', '').replace('/', ' ')
+			self.input[0] = split[1]
+		if isinstance(self.input[0], str):
+			self.input[0] = self.input[0].replace('httpd', '').replace('/', ' ')
 
 	@staticmethod
 	def on_item_pre_convert(self, item):
@@ -88,6 +88,6 @@ class searchsploit(Command):
 				item.tags = product_info + item.tags
 			# else:
 			# 	self._print(f'[bold red]{item.name} ({item.reference}) did not quite match SEARCHSPLOIT_TITLE_REGEX. Please report this issue.[/]')  # noqa: E501
-		input_tag = '-'.join(self.input.replace('\'', '').split(' '))
+		input_tag = '-'.join(self.input[0].replace('\'', '').split(' '))
 		item.tags = [input_tag] + item.tags
 		return item
