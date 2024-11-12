035abd7ad149eaa84456c837980dcbad4e29e760
0xlildoudou <67382584+0xlildoudou@users.noreply.github.com>
2024-11-12 18:26:34 +0000

    feat(tasks): add haiti

---
+++ secator/tasks/haiti.py
@@ -0,0 +1,9 @@
+from secator.decorators import task
+from secator.runners import Command
+
+@task()
+class haiti(Command):
+    cmd = 'haiti'
+
+    install_cmd = 'gem install haiti-hash'
+    install_github_handle = 'noraj/haiti'
