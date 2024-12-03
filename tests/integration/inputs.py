16826e12d65b1f3bcc91cc3efe06b3b6ad0f81c0
Olivier Cervello <ocervello@freelabz.com>
2024-12-03 12:53:05 -0500

    update

--- tests/integration/inputs.py
+++ tests/integration/inputs.py
@@ -4,7 +4,7 @@ INPUTS_TASKS = {
     URL: 'http://localhost:3000/',
     HOST: 'localhost',
     USERNAME: 'ocervell',
-    IP: 'localhost',
+    IP: '127.0.0.1',
     CIDR_RANGE: '192.168.1.0/24',
 	'bup': 'http://localhost:3000/ftp/coupons_2013.md.bak',
     'dalfox': 'http://testphp.vulnweb.com/listproducts.php?cat=123&artist=123&asdf=ff',
