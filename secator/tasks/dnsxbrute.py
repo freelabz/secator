376ab73d71a0840c687ec2428ed6aa418f604b9c
Olivier Cervello <ocervello@freelabz.com>
2025-03-06 12:01:25 -0500

    update

--- secator/tasks/dnsxbrute.py
+++ secator/tasks/dnsxbrute.py
@@ -9,31 +9,45 @@ from secator.utils import process_wordlist
 
 @task()
 class dnsxbrute(ReconDns):
-    """dnsx is a fast and multi-purpose DNS toolkit designed for running various library."""
-    cmd = 'dnsx'
-    json_flag = '-json'
-    input_flag = '-domain'
-    file_flag = '-domain'
-    opt_key_map = {
-        RATE_LIMIT: 'rate-limit',
-        RETRIES: 'retry',
-        THREADS: 'threads',
-    }
-    opts = {
-        WORDLIST: {'type': str, 'short': 'w', 'default': CONFIG.wordlists.defaults.dns, 'process': process_wordlist, 'help': 'Wordlist to use'},  # noqa: E501
-        'trace': {'is_flag': True, 'default': False, 'help': 'Perform dns tracing'},
-    }
-    item_loaders = [JSONSerializer()]
-    output_map = {
-        Subdomain: {
-            HOST: 'host',
-            DOMAIN: lambda x: ".".join(x['host'].split('.')[1:]),
-            EXTRA_DATA: lambda x: {
-                'resolver': x['resolver'],
-                'status_code': x['status_code']
+	"""dnsx is a fast and multi-purpose DNS toolkit designed for running various library."""
+	cmd = 'dnsx -resp -recon'
+	json_flag = '-json'
+	input_flag = '-domain'
+	file_flag = '-domain'
+	opt_key_map = {
+		RATE_LIMIT: 'rate-limit',
+		RETRIES: 'retry',
+		THREADS: 'threads',
+	}
+	opts = {
+		WORDLIST: {'type': str, 'short': 'w', 'default': CONFIG.wordlists.defaults.dns, 'process': process_wordlist, 'help': 'Wordlist to use'},  # noqa: E501
+		'trace': {'is_flag': True, 'default': False, 'help': 'Perform dns tracing'},
+	}
+	item_loaders = [JSONSerializer()]
+	output_map = {
+		Subdomain: {
+			HOST: 'host',
+			DOMAIN: lambda x: ".".join(x['host'].split('.')[1:]),
+			EXTRA_DATA: lambda x: {
+				'resolver': x['resolver'],
+				'status_code': x['status_code']
 			}
-        }
-    }
-    install_cmd = 'go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest'
-    install_github_handle = 'projectdiscovery/dnsx'
-    profile = 'io'
+		}
+	}
+	install_cmd = 'go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest'
+	install_github_handle = 'projectdiscovery/dnsx'
+	profile = 'io'
+
+	@staticmethod
+	def on_json_loaded(self, item):
+		print(item)
+
+		if item['status_code'] != 'NXDOMAIN':
+			yield item
+		# yield Record(
+		# 	host=host,
+		# 	name=name,
+		# 	type=_type.upper(),
+		# 	extra_data=extra_data
+		# )
+		yield item
