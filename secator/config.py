5e023e30c2fec5f3456232b93db29513f2df7d0f
Olivier Cervello <ocervello@freelabz.com>
2025-03-31 04:21:25 -0400

    allow disable celery backend updates

--- secator/config.py
+++ secator/config.py
@@ -70,7 +70,6 @@ class Celery(StrictModel):
 	task_acks_late: bool = False
 	task_send_sent_event: bool = False
 	task_reject_on_worker_lost: bool = False
-	task_live_updates: bool = True
 	worker_max_tasks_per_child: int = 20
 	worker_prefetch_multiplier: int = 1
 	worker_send_task_events: bool = False
