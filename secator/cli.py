beea030fb52bf714cb9ab888ca27afdcec1a817a
Olivier Cervello <ocervello@freelabz.com>
2025-06-05 07:02:00 -0400

    fix lint

--- secator/cli.py
+++ secator/cli.py
@@ -1485,7 +1485,7 @@ def lint(linter):
 	"""Run lint tests."""
 	opts = ''
 	if linter == 'pylint':
-		opts = '--indent-string "\t" --max-line-length 160 --disable=R0401,R0801,R0914,W0212,C0415,C0103'
+		opts = '--indent-string "\t" --max-line-length 160 --disable=R,C,W'
 	elif linter == 'ruff':
 		opts = ' check'
 	cmd = f'{sys.executable} -m {linter} {opts} secator/'
