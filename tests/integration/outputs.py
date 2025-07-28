ec7f9351042207c6ef389a3f68d624d8df81ad75
Olivier Cervello <ocervello@freelabz.com>
2025-07-28 10:49:42 -0400

    update

--- tests/integration/outputs.py
+++ tests/integration/outputs.py
@@ -289,7 +289,7 @@ OUTPUTS_TASKS = {
     ],
     'wpscan': [
         Tag(
-            name='Wordpress theme - twentytwentyfive 1.2',
+            name='Wordpress theme - twentytwentyfive 1.3',
             match='http://localhost:8000/',
             _source='wpscan'),
         Vulnerability(
