fa699e8f7336a3579ff8d2054ea50223a6cf0cc9
Olivier Cervello <ocervello@freelabz.com>
2025-11-18 19:06:44 +0100

    feat(task): jswhois integration (#742)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **New Features**
    * Introduced WHOIS domain lookup capability for comprehensive host
    information retrieval. Users can now perform targeted WHOIS queries on
    specified domains, with results delivered in JSON format and
    automatically organized with domain and informational metadata tags. The
    highly structured output enables seamless integration with existing
    reconnaissance and security assessment workflows.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ secator/tasks/jswhois.py
@@ -0,0 +1,33 @@
+from secator.decorators import task
+from secator.runners import Command
+from secator.definitions import HOST
+from secator.output_types import Tag
+from secator.serializers import JSONSerializer
+
+
+@task()
+class jswhois(Command):
+	"""WHOIS in JSON format"""
+	cmd = 'jswhois'
+	input_types = [HOST]
+	output_types = [Tag]
+	item_loaders = [JSONSerializer()]
+	tags = ['domain', 'info']
+	input_flag = None
+	file_flag = None
+	install_version = '69af013b99d49191c9674cde2e2b57986f6b6bf8'
+	install_cmd = 'go install -v github.com/jschauma/jswhois@[install_version]'
+	# install_github_handle = 'jschauma/jswhois'
+
+	@staticmethod
+	def on_json_loaded(self, item):
+		last_chain = item['chain'][-1]
+		last_elem = item[last_chain]
+		raw = last_elem.pop('raw')
+		tag = Tag(
+			name=f'{self.inputs[0]} WHOIS',
+			category='whois',
+			match=self.inputs[0],
+			extra_data={'content': raw, 'chain': last_chain}
+		)
+		yield tag
