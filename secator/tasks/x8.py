994357a5c297b6425062efffe6711008b082927f
Olivier Cervello <ocervello@freelabz.com>
2025-11-19 16:45:57 -0500

    update

--- secator/tasks/x8.py
+++ secator/tasks/x8.py
@@ -57,9 +57,9 @@ class x8(HttpFuzzer):
 	}
 	install_version = '4.3.0'
 	install_cmd = 'cargo install x8@[install_version] --force'
-	install_github_handle = 'Sh1Yo/x8'
-	install_github_version_prefix = 'v'
-	install_ignore_bin = ['alpine', 'ubuntu']
+	# install_github_handle = 'Sh1Yo/x8'
+	# install_github_version_prefix = 'v'
+	# install_ignore_bin = ['alpine', 'ubuntu']
 	proxychains = False
 	proxy_socks5 = False
 	proxy_http = False
