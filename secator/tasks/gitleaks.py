8f2fe621e5ba463604fcb88570a565d439b47492
Olivier Cervello <ocervello@freelabz.com>
2025-11-28 11:02:15 +0100

    fix(install): exact version of gitleaks when installing from source (#800)

--- secator/tasks/gitleaks.py
+++ secator/tasks/gitleaks.py
@@ -41,10 +41,10 @@ class gitleaks(Command):
 	}
 	input_type = "folder"
 	output_types = [Tag]
-	install_version = 'v8.24.3'
+	install_version = 'v8.29.1'
 	install_cmd_pre = {'*': ['git', 'make']}
 	install_cmd = (
-		f'git clone https://github.com/gitleaks/gitleaks.git {CONFIG.dirs.share}/gitleaks_[install_version] || true &&'
+		f'git clone --single-branch -b [install_version] https://github.com/gitleaks/gitleaks.git {CONFIG.dirs.share}/gitleaks_[install_version] || true &&'  # noqa: E501
 		f'cd {CONFIG.dirs.share}/gitleaks_[install_version] && make build &&'
 		f'mv {CONFIG.dirs.share}/gitleaks_[install_version]/gitleaks {CONFIG.dirs.bin}'
 	)
