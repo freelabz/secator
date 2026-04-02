ad22cf979b9941331f8377d0b48a47a3a8370cb5
Olivier Cervello <ocervello@freelabz.com>
2026-04-02 14:53:32 +0200

    fix: tab

--- secator/output_types/ai.py
+++ secator/output_types/ai.py
@@ -125,7 +125,7 @@ class Ai(OutputType):
 			action_label = self.ai_type
 			if self.ai_type == 'stopped':
 				action_label = 'done'
-            action_label_str = action_label.capitalize().replace('_', ' ')
+			action_label_str = action_label.capitalize().replace('_', ' ')
 			line = f'{s}[bold blue]{action_label_str}[/]'
 			content = _s(self.content)
 			if self.ai_type in ['task', 'workflow', 'scan']:
